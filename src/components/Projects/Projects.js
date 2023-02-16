01903529526b6c99ea17a1e833152fb5e4b3b445
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2023-02-16 22:14:28 +0000

    projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -45,6 +45,7 @@ const Projects = () => (
             </div>
             <UtilityList>
               <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={source}>Website</ExternalLinks>
             </UtilityList>
           </BlogCard>
         )
