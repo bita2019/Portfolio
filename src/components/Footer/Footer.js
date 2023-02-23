7ca76d4db3532ddcad90a30c04d3fb58587254dc
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2023-02-23 22:21:53 +0000

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Emaile</LinkTitle>
+          <LinkItem href="mailto:mohsmaryam@gmail.com">
+            mohsmaryam@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <CompanyContainer>
+        <SocialIcons href="https://github.com/bita2019">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/maryam-mohsenpour-07632a16b">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+      </CompanyContainer>
+    </FooterWrapper>
   );
 };
 
